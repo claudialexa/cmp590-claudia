64dbab125e8b178c97de8e1adb46a852dca790c0
claudia aguirre <claudialexaa@gmail.com>
2015-05-03 20:17:49 -0400

    Uploading Everything
    hi

---
+++ Homework/javascript/js/main.js
@@ -0,0 +1,99 @@
+// max of two
+
+function max(a, b) {
+	if (a > b) {
+		return a;
+	}
+	else {
+		return b;
+	}
+}
+
+
+
+// max of three function
+
+// function maxOfThree(x, y, z) {
+// 	if (x> y) {
+// 		if (x > z) {
+// 			return x;
+// 		}
+// 		else {
+// 			return z;
+// 		}
+// 	}
+// 	else if (y > z) {
+// 		return y;
+// 	}
+// 	else {
+// 		return z;
+// 	}
+// }
+
+// // check for vowels
+
+// function trueVowel(str) {
+// 	switch (str) {
+// 		case "a":
+// 			return true;
+// 			break;
+// 		case "e":
+// 			return true;
+// 			break;
+// 		case "i":
+// 			return true;
+// 			break;
+// 		case "o":
+// 			return true;
+// 			break;
+// 		case "u":
+// 			return true;
+// 			break;
+// 		default:
+// 			return false;
+// 	}
+// }
+
+// // pig latin
+
+
+// // sum and multiple
+
+// function sum(array) {
+// 	return array.reduce(function(2, 2, 5) {
+// 		return 2 + 2 + 5;
+// 	}
+// 			console.log(result);
+
+// }
+ 
+// function multiply(array) {
+// 	return array.reduce(function(2, 2, 5) {
+// 		return 2 * 2 * 5;
+// 	})
+// }
+
+// video reverse
+
+// function reverse(string) {
+// 	var newStr = " ";
+// 	var string = "video"
+// 	for (var i = string.length - 1; i >= 0; i--) {
+// 		newStr += string[i];
+// 	}
+// 	return newStr;
+// }
+
+// // longest word
+
+// function findLongestWord(array) {
+// 	var longestWord = ['rodeo', 'potato', 'tomato'];
+// 	var longestWord = array[0];
+// 	for (var i = 1; i < array.length; i++) {
+// 		if (longestWord.length < array[i].length) {
+// 			longestWord = array[i];
+// 		}
+// 	}
+// 	return longestWord.length;
+// }
+
